6d1e921a4fb4f73444b3905b288936ab0ff1c789
MariyaMah <72305708+MariyaMah@users.noreply.github.com>
2023-01-31 22:02:31 +0300

    Add files via upload

---
+++ включить/kontakt_ap.php
@@ -0,0 +1,33 @@
+    <div class="ru-en">
+        <a href="" class="">RU</a>
+        <span class="">|</span>
+        <a href="" class="">EN</a> 
+    </div>
+
+    <div class="messag">
+        <div class="whatsapp">
+        <a href="https://wa.me/7921177603"><img src="//mysite/image/whatsappwhite.svg" alt="Написать в Whatsapp"></a>
+    </div>
+
+    <div class="telegram">
+        <a href="https://telegram.me/Mariya_Makhina"><img src="//mysite/image/telegrammwwhite.svg" alt="Написать в Telegram"></a>
+    </div>
+
+    <div class="viber">
+        <a href="viber://chat?number=%2B7921177603"><img src="//mysite/image/viberwhite.svg" alt="Написать в Viber"></a>
+    </div>
+
+    <div class="telef">
+        <a href="tel:+7921177603"><img src="//mysite/image/phonewhite.svg" alt="Позвонить"></a>
+    </div>
+
+    <div class="emailk">
+        <a href="mailto:mascha.vas@gmail.com"><img src="//mysite/image/mailwhite.svg" alt="Написать на email"></a>
+    </div>
+</div>
+
+<div class="resume_div">
+    <a class="doc_img" href="#" id="zatemnenie">
+        	<iframe id="oknosv" src="..\doc\resume.pdf"></iframe>
+    <a class="res_text" href="#zatemnenie">Резюме</a></a>
+</div>
---
+++ включить/menu.php
@@ -0,0 +1,15 @@
+
+    <ul class="navmy" >
+		<li class="not_left" <?php if ($punkt == '#contElements') {echo 'class="navmy-punkt"';} ?> > <a href="#contElements">Элементы интерфейса</a></li>	
+		<span class="vertik_dark"></span><span class="vertik_white"></span>
+		<li  <?php if ($punkt == '#contProjectSite') {echo 'class="navmy-punkt"';} ?> > <a href="#contProjectSite">Проекты. Сайты</a></li>	
+		<span class="vertik_dark"></span><span class="vertik_white"></span>
+		<li <?php if ($punkt == '#contMe') {echo 'class ="navmy-punkt"';} ?>> <a  href="#contMe">Обо мне</a>  </li>
+		<span class="vertik_dark"></span><span class="vertik_white"></span>
+		<li class="not_right" <?php if ($punkt == '#contBack') {echo 'class="navmy-punkt"';} ?> >  <a href="#contBack">Обратная связь</a>  </li>
+	</ul> 
+
+
+
+	
+
